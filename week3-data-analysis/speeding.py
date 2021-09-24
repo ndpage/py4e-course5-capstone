1a5da5ca946a63e02149963d0e150df0af397cd1
Page Nathan (DC-MH/ENG1.13-NA) <Nathan.Page@boschrexroth-us.com>
2021-09-24 17:12:18 -0400

    Added speed and period data extraction from dataframe

--- week3-data-analysis/speeding.py
+++ week3-data-analysis/speeding.py
@@ -14,7 +14,40 @@ fname = input("Enter filename: ")
 if len(fname) < 1:
     fname = "amis.csv"
 
-data = pd.read_csv(fname)   # Read data into a pandas DataFrame
+datafrm = pd.read_csv(fname)   # Read csv data into a pandas DataFrame
+#print(datafrm)
 
-print(type(data))
-print(data)
+# print(datafrm.mean(axis=0))
+
+speedfrm = datafrm['speed']  # extract speed column from data frame
+speed_list = speedfrm.tolist() #   and convert it to a list
+
+period_frame = datafrm['period']
+period_list = period_frame.tolist()
+
+length = 0 
+pre_sign = list()
+shortly_after = list()
+long_time_after = list()
+
+
+# extract list of speeds for each period (1,2, or 2) and store values in new lists
+for speed,period in zip(speed_list,period_list):
+    if period == 1: # 1: before sign was set up
+        pre_sign.append(speed)
+    elif period == 2:  # 2: shortly after sign 
+        shortly_after.append(speed)
+    else:
+        long_time_after.append(speed) # 3: a long time after sign
+
+# calculate average/mean speed of each period
+avg1 = sum(pre_sign)/len(pre_sign)
+avg2 = sum(shortly_after)/len(shortly_after)
+avg3 = sum(long_time_after)/len(long_time_after)
+
+print(''' 
+Average speed before sign {}
+Average speed shortly after sign {}
+Average speed a long time after sign {}
+'''.format(avg1,avg2,avg3))
+
