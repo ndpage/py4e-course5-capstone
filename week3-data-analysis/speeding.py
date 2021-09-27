06ffc5227f3356471dc8d4c07c922da61023330e
Page Nathan (DC-MH/ENG1.13-NA) <Nathan.Page@boschrexroth-us.com>
2021-09-27 11:17:35 -0400

    Added subplots of histograms for each period but 3rd subplot is not displaying data

--- week3-data-analysis/speeding.py
+++ week3-data-analysis/speeding.py
@@ -4,9 +4,10 @@ Desc: Analysis of the effectivenes of speed warning signs. Data read from csv fi
 Created: September 24, 2021
 '''
 
-
+from numpy import short
 import pandas as pd
 from pandas.core.frame import DataFrame
+import matplotlib.pyplot as plt
 
 # Read data from csv file 
 fname = input("Enter filename: ")
@@ -15,21 +16,16 @@ if len(fname) < 1:
     fname = "amis.csv"
 
 datafrm = pd.read_csv(fname)   # Read csv data into a pandas DataFrame
-#print(datafrm)
 
-# print(datafrm.mean(axis=0))
-
-speedfrm = datafrm['speed']  # extract speed column from data frame
-speed_list = speedfrm.tolist() #   and convert it to a list
-
+speed_frame = datafrm['speed']  # extract speed column from data frame
 period_frame = datafrm['period']
+
+speed_list = speed_frame.tolist() #   and convert it to a list
 period_list = period_frame.tolist()
 
-length = 0 
 pre_sign = list()
 shortly_after = list()
 long_time_after = list()
-
 
 # extract list of speeds for each period (1,2, or 2) and store values in new lists
 for speed,period in zip(speed_list,period_list):
@@ -37,8 +33,10 @@ for speed,period in zip(speed_list,period_list):
         pre_sign.append(speed)
     elif period == 2:  # 2: shortly after sign 
         shortly_after.append(speed)
-    else:
+    elif period == 3:
         long_time_after.append(speed) # 3: a long time after sign
+    else:
+        continue
 
 # calculate average/mean speed of each period
 avg1 = sum(pre_sign)/len(pre_sign)
@@ -51,3 +49,23 @@ Average speed shortly after sign {}
 Average speed a long time after sign {}
 '''.format(avg1,avg2,avg3))
 
+# Plot 1 
+plt.hist(pre_sign, label="Before sign")
+plt.subplot(131,label="Pre-Sign")
+plt.ylabel("Number of drivers")
+plt.xlabel("Speed (mph)")
+plt.title("Before Sign")
+
+# plot 2 
+plt.hist(shortly_after)
+plt.subplot(132, label="After sign")
+plt.xlabel("Speed (mph)")
+plt.title("Shortly After Sign")
+
+# Plot 3
+plt.hist(long_time_after)
+plt.subplot(133, label="Long time after sign")
+plt.xlabel("Speed (mph)")
+plt.title("A Long Time After Sign")
+
+plt.show()
