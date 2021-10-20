e08d81d75124c80f21a55cb46ac29d8597492211
Nathan Page <33336363+ndpage@users.noreply.github.com>
2021-10-19 22:47:48 -0400

    Commented out plt

--- week3-data-analysis/speeding.py
+++ week3-data-analysis/speeding.py
@@ -56,14 +56,14 @@ def main():
 
     # Create plots for each time period
 
-    plt.hist(pre_sign,color='blue', bins=20, label='Before sign')
-    plt.hist(shortly_after,color='green', bins=20, label='After sign')
-    plt.hist(long_time_after,color='red', bins=20, label='Long time after')
+    # plt.hist(pre_sign,color='blue', bins=20, label='Before sign')
+    # plt.hist(shortly_after,color='green', bins=20, label='After sign')
+    # plt.hist(long_time_after,color='red', bins=20, label='Long time after')
     
-    # Set axes parameters
-    plt.xlabel('Speed range')
-    plt.ylabel('Speed')
-    plt.legend()
+    # # Set axes parameters
+    # plt.xlabel('Speed range')
+    # plt.ylabel('Speed')
+    # plt.legend()
 
     plt.show()
 
